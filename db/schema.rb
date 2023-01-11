fb5582650f1ed776314dbdc5cf4ee075806d8191
EC2 Default User <ec2-user@ip-172-31-46-47.ap-northeast-1.compute.internal>
2023-01-11 00:41:13 +0000

    [Add]デプロイの開発環境での準備

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,223 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_01_10_214324) do
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+  end
+
+  create_table "game_comments", force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.integer "game_review_id", null: false
+    t.text "game_comment", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_review_id"], name: "index_game_comments_on_game_review_id"
+    t.index ["member_id"], name: "index_game_comments_on_member_id"
+  end
+
+  create_table "game_likes", force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.integer "game_review_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_review_id"], name: "index_game_likes_on_game_review_id"
+    t.index ["member_id"], name: "index_game_likes_on_member_id"
+  end
+
+  create_table "game_review_tags", force: :cascade do |t|
+    t.integer "game_tag_id", null: false
+    t.integer "game_review_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_review_id"], name: "index_game_review_tags_on_game_review_id"
+    t.index ["game_tag_id"], name: "index_game_review_tags_on_game_tag_id"
+  end
+
+  create_table "game_reviews", force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.integer "genre_id", null: false
+    t.string "game_title", null: false
+    t.string "game_tag"
+    t.text "game_summary", null: false
+    t.text "game_impression", null: false
+    t.integer "game_price", null: false
+    t.float "game_rating", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_game_reviews_on_genre_id"
+    t.index ["member_id"], name: "index_game_reviews_on_member_id"
+  end
+
+  create_table "game_tags", force: :cascade do |t|
+    t.string "game_tag_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "genre_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "goods_comments", force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.integer "goods_review_id", null: false
+    t.text "goods_comment", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["goods_review_id"], name: "index_goods_comments_on_goods_review_id"
+    t.index ["member_id"], name: "index_goods_comments_on_member_id"
+  end
+
+  create_table "goods_likes", force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.integer "goods_review_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["goods_review_id"], name: "index_goods_likes_on_goods_review_id"
+    t.index ["member_id"], name: "index_goods_likes_on_member_id"
+  end
+
+  create_table "goods_review_tags", force: :cascade do |t|
+    t.integer "goods_tag_id", null: false
+    t.integer "goods_review_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["goods_review_id"], name: "index_goods_review_tags_on_goods_review_id"
+    t.index ["goods_tag_id"], name: "index_goods_review_tags_on_goods_tag_id"
+  end
+
+  create_table "goods_reviews", force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.string "goods_name", null: false
+    t.string "goods_tag"
+    t.text "goods_summary", null: false
+    t.text "goods_impression", null: false
+    t.integer "goods_price", null: false
+    t.float "goods_rating", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["member_id"], name: "index_goods_reviews_on_member_id"
+  end
+
+  create_table "goods_tags", force: :cascade do |t|
+    t.string "goods_tag_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "group_chats", force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.integer "group_id", null: false
+    t.text "group_chat", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_group_chats_on_group_id"
+    t.index ["member_id"], name: "index_group_chats_on_member_id"
+  end
+
+  create_table "group_members", force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.integer "group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_group_members_on_group_id"
+    t.index ["member_id"], name: "index_group_members_on_member_id"
+  end
+
+  create_table "group_rooms", force: :cascade do |t|
+    t.integer "room_owner_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.integer "game_review_id", null: false
+    t.integer "group_owner_id", null: false
+    t.string "group_name", null: false
+    t.text "group_introduction", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_review_id"], name: "index_groups_on_game_review_id"
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "member_name", null: false
+    t.string "member_introduction"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "room_chats", force: :cascade do |t|
+    t.integer "group_room_id", null: false
+    t.integer "group_member_id", null: false
+    t.text "room_chat", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_member_id"], name: "index_room_chats_on_group_member_id"
+    t.index ["group_room_id"], name: "index_room_chats_on_group_room_id"
+  end
+
+  create_table "room_members", force: :cascade do |t|
+    t.integer "group_member_id", null: false
+    t.integer "group_room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_member_id"], name: "index_room_members_on_group_member_id"
+    t.index ["group_room_id"], name: "index_room_members_on_group_room_id"
+  end
+
+  add_foreign_key "follows", "followeds"
+  add_foreign_key "follows", "followers"
+  add_foreign_key "game_comments", "game_reviews"
+  add_foreign_key "game_comments", "members"
+  add_foreign_key "game_likes", "game_reviews"
+  add_foreign_key "game_likes", "members"
+  add_foreign_key "game_review_tags", "game_reviews"
+  add_foreign_key "game_review_tags", "game_tags"
+  add_foreign_key "game_reviews", "genres"
+  add_foreign_key "game_reviews", "members"
+  add_foreign_key "goods_comments", "goods_reviews"
+  add_foreign_key "goods_comments", "members"
+  add_foreign_key "goods_likes", "goods_reviews"
+  add_foreign_key "goods_likes", "members"
+  add_foreign_key "goods_review_tags", "goods_reviews"
+  add_foreign_key "goods_review_tags", "goods_tags"
+  add_foreign_key "goods_reviews", "members"
+  add_foreign_key "group_chats", "groups"
+  add_foreign_key "group_chats", "members"
+  add_foreign_key "group_members", "groups"
+  add_foreign_key "group_members", "members"
+  add_foreign_key "groups", "game_reviews"
+  add_foreign_key "room_chats", "group_members"
+  add_foreign_key "room_chats", "group_rooms"
+  add_foreign_key "room_members", "group_members"
+  add_foreign_key "room_members", "group_rooms"
 end
