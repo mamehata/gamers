ca4e4e0075471bf515051565868b3b98f5b7c12f
EC2 Default User <ec2-user@ip-172-31-46-47.ap-northeast-1.compute.internal>
2023-01-11 00:09:04 +0000

    [Update]routes.rb編集

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,47 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root to: "homes#top"
+    resources :members, except: [:new, :create, :update]
+    resources :goods_reviews, only: [:index, :show, :destroy] do
+      resources :goods_comments, only: [:destroy]
+    end
+    resources :goods_reviews, only: [:index, :show, :destroy] do
+      resources :game_comments, only: [:destroy]
+    end
+    resources :genres, except: [:new, :show, :destroy]
+  end
 
+  scope model: :public do
+    root to: "homes#top"
+    get "about" => "homes#about"
+    resources :members, except: [:new, :create] do
+      member do
+        get "confirmation"
+      end
+      resources :follows, only: [:create, :destroy] do
+        member do
+          get "followeds"
+          get "followers"
+        end
+      end
+    end
+    resources :goods_reviews, except: [:new, :edit, :update] do
+      resources :goods_likes, only: [:create, :destroy]
+      resources :goods_comments, only: [:create, :update, :destroy]
+    end
+    resources :game_reviews, except: [:new, :edit, :update] do
+      resources :game_likes, only: [:create, :destroy]
+      resources :game_comments, only: [:create, :update, :destroy]
+    end
+    resources :groups, except: [:new, :show, :index] do
+      resources :group_members, only: [:create, :destroy]
+      resources :group_chats, only: [:create, :update, :destroy]
+    end
+    resources :group_rooms, only: [:create, :destroy] do
+      resources :room_members, only: [:create, :destroy]
+      resources :room_chats, only: [:create, :update, :destroy]
+    end
+  end
 end
