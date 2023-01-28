6b81685c3fe929a6e870bdc89df849001f2aac9e
EC2 Default User <ec2-user@ip-172-31-46-47.ap-northeast-1.compute.internal>
2023-01-28 19:53:37 +0000

    [Add]グループチャット通知機能実装

--- app/models/member.rb
+++ app/models/member.rb
@@ -21,6 +21,7 @@ class Member < ApplicationRecord
   has_many :reverse_of_follows, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :follows, source: :followed
   has_many :followers, through: :reverse_of_follows, source: :follower
+  has_many :notifications, dependent: :destroy
 
   validates :member_name, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_14_065155) do
+ActiveRecord::Schema.define(version: 2023_01_28_150143) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -189,6 +189,15 @@ ActiveRecord::Schema.define(version: 2023_01_14_065155) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.integer "group_id", null: false
+    t.integer "group_chat_id", null: false
+    t.boolean "see", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "room_chats", force: :cascade do |t|
